ea2f08f607aaee7fa1b620ceb6c3f08f032fbd26
jp_singh34 <nirankarijp@gmail.com>
2022-09-02 16:31:08 +0530

    added comments

--- src/main/java/com/example/Springbootpractice/Service/DepartmentServiceImp.java
+++ src/main/java/com/example/Springbootpractice/Service/DepartmentServiceImp.java
@@ -82,7 +82,7 @@ public class DepartmentServiceImp implements DepartmentService{
 return departmentRepository.findAll();
     }
 
-    //this is for our custom data entity search yeah
+    //this is for our custom data entity search yeah okay
     @Override
     public Department getDepartmentByName(String departmentName) {
         return departmentRepository.findByDepartmentNameIgnoreCase(departmentName);
